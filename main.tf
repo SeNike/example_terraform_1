573d823ebc576d3da039aea8efbf383d8a18e51c
SeNike <nsvtemp@gmail.com>
2024-08-30 11:02:20 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+  required_version = "~>1.8.4" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+
+
+variable "ssh_conn" {
+  description = "name of VM user"
+  type        = string
+}
+
+
+variable "mysql_root_password" {
+  description = "mysql root password"
+  type        = string
+}
+
+variable "mysql_password" {
+  description = "mysql password"
+  type        = string
+}
+
+
+#однострочный комментарий
+provider "docker" {host = var.ssh_conn}
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "docker_image" "mysql" {
+  name = "mysql:8.0"
+}
+
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name  = "example_${random_password.random_string.result}"
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${var.mysql_password}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+  ports {
+    internal = 3306
+    external = 3306
+    ip       = "127.0.0.1"
+  }
+}
